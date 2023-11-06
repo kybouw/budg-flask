41014eb093c30127133e088111b9cad1fb23c2d6
Kyle Bouwman <kybouw@users.noreply.github.com>
2023-11-06 04:34:24 +0000

    make calc endpoint return text not template

--- budg/app.py
+++ budg/app.py
@@ -7,15 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template("submit.html")
 
-@app.route("/calculate")
+@app.route("/calculate", methods=["POST"])
 def calculate():
     plan = request.args.get("plan")
     amount = request.args.get("amount")
     amount_val = get_dollar_value(amount)
     table = budg_calculate(plan, amount_val)
     table_lines = table.split("\n")
+    return str.join("<br>", table_lines)
 
-    return render_template("result.html", table_lines=table_lines)
+    # return render_template("result.html", table_lines=table_lines)
 
 if __name__ == "__main__":
     app.run(debug=True)
